f692d218c6ec2fa3e571d49f1580ca3b4303e9e7
Alexey Maltsev <aamaltsev@skbkontur.ru>
2018-10-30 11:23:58 +0500

    Stated adding styling

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,11 @@ const Header = ({ siteTitle }) => (
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ 
+            margin: 0,
+            borderBottomWidth: 0,
+          }}
+        >
         <Link
           to="/"
           style={{
