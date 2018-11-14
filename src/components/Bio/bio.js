bc1b2eeed00d5dc9d2c223c5cd098b242066166e
Alexey Maltsev <aamaltsev@skbkontur.ru>
2018-11-14 10:10:10 +0500

    Changed text for bio

--- src/components/Bio/bio.js
+++ src/components/Bio/bio.js
@@ -7,7 +7,7 @@ const Bio = ({itemProp}) => (
     <img src={ProfileImage} className={styles.photo} alt={'me in the mountains'} itemProp={"image"}/>
     <div className={styles.info}>
       <p>Blog of <b itemProp={"name"}>Alexey Maltsev</b>, full-stack developer from Russia.
-      Here published some cool stuff about web and mobile development with .Net.</p>
+      Most of the time he works with .Net-projects for web and mobile.</p>
     </div>
   </div>
 )
