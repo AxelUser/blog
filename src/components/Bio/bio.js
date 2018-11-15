cce1edbca21c27ac87a85efca012d16ad0e209a4
Alexey Maltsev <aamaltsev@skbkontur.ru>
2018-11-15 21:18:50 +0500

    Moved social links to Bio

--- src/components/Bio/bio.js
+++ src/components/Bio/bio.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import styles from './bio.module.less';
 import ProfileImage from '../../images/profile.jpg'
+import TwitterIcon from '../../images/social/twitter.svg'
+import GitHubIcon from '../../images/social/github.svg'
+import LinkedInIcon from '../../images/social/linkedin.svg'
 
 const Bio = ({itemProp}) => (
-  <div itemProp={itemProp} className = {styles.container} itemScope itemType={"http://schema.org/Person"}>
-    <img src={ProfileImage} className={styles.photo} alt={'me in the mountains'} itemProp={"image"}/>
-    <div className={styles.info}>
-      <p>Blog of <b itemProp={"name"}>Alexey Maltsev</b>, full-stack developer from Russia.
-      Most of the time he works with .Net-projects for web and mobile.</p>
+  <div className={styles.container}>
+    <div className={styles.profile} itemProp={itemProp} itemScope itemType={"http://schema.org/Person"}>
+      <img src={ProfileImage} className={styles.photo} alt={'me in the mountains'} itemProp={"image"}/>
+      <div className={styles.info}>
+        <p>Blog of <b itemProp={"name"}>Alexey Maltsev</b>, full-stack developer from Russia.
+        Most of the time he works with .Net-projects for web and mobile.</p>
+      </div>
+    </div>
+    <div className={styles.socialBlock}>
+      <a href="https://linkedin.com/in/dev-alexey-maltsev">
+        <LinkedInIcon className={styles.socialIcon}/>
+      </a>
+      <a href="https://github.com/AxelUser">
+        <GitHubIcon className={styles.socialIcon}/>
+      </a>
+      <a href="https://twitter.com/axel_user">
+        <TwitterIcon className={styles.socialIcon}/>
+      </a>
     </div>
   </div>
 )
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { StaticQuery, graphql } from 'gatsby'
 import styles from './header.module.less'
-import TwitterIcon from '../../images/social/twitter.svg'
-import GitHubIcon from '../../images/social/github.svg'
-import LinkedInIcon from '../../images/social/linkedin.svg'
 
 const Header = () => (
   <StaticQuery 
@@ -43,17 +40,6 @@ const HeaderForTitle = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-      <div className={styles.socialBlock}>
-        <a href="https://linkedin.com/in/dev-alexey-maltsev">
-          <LinkedInIcon className={styles.socialIcon}/>
-        </a>
-        <a href="https://github.com/AxelUser">
-          <GitHubIcon className={styles.socialIcon}/>
-        </a>
-        <a href="https://twitter.com/axel_user">
-          <TwitterIcon className={styles.socialIcon}/>
-        </a>
-      </div>
     </div>
   </div>
 )
