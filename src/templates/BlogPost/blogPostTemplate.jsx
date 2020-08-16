d3c09cc8462f2864470fc8dd1139b46957bf525d
Alexey Maltsev <axel.user.perm@gmail.com>
2020-08-16 23:12:47 +0300

    Created css class for blog post layout

--- src/templates/BlogPost/blogPostTemplate.jsx
+++ src/templates/BlogPost/blogPostTemplate.jsx
@@ -26,27 +26,29 @@ const BlogPostTemplate = ({location, pageContext, data: {
 	var {next, prev} = pageContext;
 	return (
 		<Layout>
-			<div className={styles.postBlock}>
-				<SEO title = {title} description={excerpt}/>
-				<PublicationSEO publicationDate={date} tags={keywords}/>
-				<article className={styles.post} itemScope itemType={'http://schema.org/CreativeWork'}>
-					<meta itemProp={"description"} content={preview}/>
-					<meta itemProp="keywords" content={keywords.join(", ")} />
-					<span className={styles.meta}>
-						<time itemProp={'datePublished'}>{date}</time>
-						<span>~{timeToRead} min to read</span>
-						{keywords.map((tag, i) => <span key={i}>[{tag}]</span>)}
-					</span>
-					<h1 className={styles.title} itemProp={'headline'}>{title}</h1>
-					<div className={styles.text} itemProp={'text'} dangerouslySetInnerHTML = {{__html: html}} />
-					<div className={styles.share}>
-						<SocialSharing shareUrl={location.href} title={title} description={preview} tags={tags}/>
-					</div>
-					<FooterNavigation prev={prev} next={next}/>				
-				</article>
-				<Bio itemProp={"author"}/>
+			<div className={styles.postLayoutBlock}>
+				<div className={styles.postContentBlock}>
+					<SEO title = {title} description={excerpt}/>
+					<PublicationSEO publicationDate={date} tags={keywords}/>
+					<article className={styles.post} itemScope itemType={'http://schema.org/CreativeWork'}>
+						<meta itemProp={"description"} content={preview}/>
+						<meta itemProp="keywords" content={keywords.join(", ")} />
+						<span className={styles.meta}>
+							<time itemProp={'datePublished'}>{date}</time>
+							<span>~{timeToRead} min to read</span>
+							{keywords.map((tag, i) => <span key={i}>[{tag}]</span>)}
+						</span>
+						<h1 className={styles.title} itemProp={'headline'}>{title}</h1>
+						<div className={styles.text} itemProp={'text'} dangerouslySetInnerHTML = {{__html: html}} />
+						<div className={styles.share}>
+							<SocialSharing shareUrl={location.href} title={title} description={preview} tags={tags}/>
+						</div>
+						<FooterNavigation prev={prev} next={next}/>				
+					</article>
+					<Bio itemProp={"author"}/>
+				</div>
+				<SidebarNavigation headers={["Header 1", "Header 2", "Header 3", "Header 4"]}/>
 			</div>
-			<SidebarNavigation headers={["Header 1", "Header 2", "Header 3", "Header 4"]}/>
 		</Layout>
 	)
 }
