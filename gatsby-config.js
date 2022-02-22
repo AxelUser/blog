83690b2a6d33203db17e953db03a13ecaf15ae4b
Alexey Maltsev <axel.user.perm@gmail.com>
2022-02-22 22:41:50 +0300

    Added syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,48 @@
 module.exports = {
-  siteMetadata: {
-      title: `Maltsev's Space`,
-      siteUrl: `https://www.maltsev.space`
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet", 
-    "gatsby-plugin-sitemap", {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        "icon": "src/images/icon.png"
-      }
-    },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp", {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "images",
-        "path": "./src/images/"
-      },
-      __key: "images"
-    }, {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "pages",
-        "path": "./src/pages/"
-      },
-      __key: "pages"
-    }, {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "blog",
-        "path": "./content/"
-      },
-      __key: "blog"
-    }, "gatsby-plugin-emotion"
-  ]
+	siteMetadata: {
+		title: `Maltsev's Space`,
+		siteUrl: `https://www.maltsev.space`
+	},
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap", {
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				"icon": "src/images/icon.png"
+			}
+		}, {
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-prismjs`
+					}
+				]
+			}
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp", {
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				"name": "images",
+				"path": "./src/images/"
+			},
+			__key: "images"
+		}, {
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				"name": "pages",
+				"path": "./src/pages/"
+			},
+			__key: "pages"
+		}, {
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				"name": "blog",
+				"path": "./content/"
+			},
+			__key: "blog"
+		}
+	]
 };
