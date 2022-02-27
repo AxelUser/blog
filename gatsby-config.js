f81766b6fa38197474754220b07a5ac35c8c890b
Alexey Maltsev <axel.user.perm@gmail.com>
2022-02-27 22:56:07 +0300

    Updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,8 @@ module.exports = {
 		"gatsby-plugin-sitemap", {
 			resolve: 'gatsby-plugin-manifest',
 			options: {
+				"name": "Maltsev's Space",
+				"start_url": "/",
 				"icon": "src/images/icon.png"
 			}
 		}, {
