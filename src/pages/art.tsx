ba9c9c3315d38457359980f61fedc65899ad9d12
Alexey Maltsev <axel.user.perm@gmail.com>
2024-02-21 23:45:10 +0100

    added more images

--- src/pages/art.tsx
+++ src/pages/art.tsx
@@ -1,24 +1,38 @@
 import { HeadFC, PageProps, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../components/layout"
 import { Seo } from "../components/seo"
 
-const ArtPage: React.FC<PageProps<Queries.ArtImagesQuery>> = ({
-  data: {
-    allFile: { nodes: images },
-  },
-}) => (
-  <Layout>
-    <h1>AI Art</h1>
+type GalleryProps = {
+  images: ImageData[]
+}
+
+type ImageData = {
+  name: string
+  image: IGatsbyImageData
+}
+
+const Gallery: React.FC<GalleryProps> = ({ images }) => (
+  <div>
     {images.map(img => (
-      <GatsbyImage
-        alt={img.name}
-        image={img.childImageSharp?.gatsbyImageData!}
-      />
+      <GatsbyImage alt={img.name} image={img.image} />
     ))}
-  </Layout>
+  </div>
 )
+
+const ArtPage: React.FC<PageProps<Queries.ArtImagesQuery>> = ({ data }) => {
+  const images: ImageData[] = data.allFile.nodes.map(n => ({
+    name: n.name,
+    image: n.childImageSharp!.gatsbyImageData,
+  }))
+  return (
+    <Layout>
+      <h1>AI Art</h1>
+      <Gallery images={images} />
+    </Layout>
+  )
+}
 
 export default ArtPage
 
@@ -32,7 +46,9 @@ export const query = graphql`
       nodes {
         name
         childImageSharp {
-          gatsbyImageData
+          gatsbyImageData(
+            transformOptions: { cropFocus: ATTENTION, fit: INSIDE }
+          )
         }
       }
     }
